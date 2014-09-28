6d2a17d4c319b25c6adbac3c20003d2ff0e45f79
RandWilliams <j.rand.williams@gmail.com>
2014-09-27 20:03:44 -0400

    RandStuff
    RandStuff

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+	public NodeManager gameManager = null;
+	public Camera camera = null;
+	public Vector3 center = new Vector3 (0f, 0f);
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// global mode -- center at global extent center
+		if ((gameManager != null) && (camera != null)) {
+			camera.rect = gameManager.Extents;
+		}
+	}
+}
--- Assets/Scripts/NodeManager.cs
+++ Assets/Scripts/NodeManager.cs
@@ -11,10 +11,16 @@ public class NodeManager : MonoBehaviour {
 	public GameObject GameNode;
 	public GameObject GameEdge;
 
+	public Rect Extents = new Rect (0f, 0f, 0f, 0f);
+	public float Aspect = 16f / 9f ; 
+	public float Scale  = 100; // <-- Camera Size
+	public float Adjust = 2.0f;
+
 	// Use this for initialization
 	void Start () {
 		for(int i = 0; i < NodeCount; ++i){
-			Instantiate(GameNode, Util.RandomVector2(2), Quaternion.identity );
+			Vector2 limit = new Vector2( (Random.value - 0.5f) * Scale * Aspect * Adjust, (Random.value - 0.5f) * Scale * Adjust);
+			Instantiate(GameNode, limit, Quaternion.identity );
 		}
 
 		RelationMatrix = new RelationMatrix(GameObject.FindObjectsOfType<Node>());
@@ -31,17 +37,27 @@ public class NodeManager : MonoBehaviour {
 	void Update () {
 //		Nodes[0].velocity += Nodes[0].RepulsiveForce(Nodes[1]);
 //		Nodes[0].velocity += Nodes[0].RepulsiveForce(Nodes[2]);
+
 		for (int i = 1; i < Nodes.Count; ++i) {
 			Node node = Nodes[i];
-			for(int j = 0; j < OldNodes.Count; ++j) {
+			for(int j = 0; j < OldNodes.Count;	 ++j) {
 				node.velocity += node.RepulsiveForce(OldNodes[j]);	
 				if(RelationMatrix[i,j])
 					node.velocity += node.AttractiveForce(OldNodes[j]);
 			}
+
+			Vector3 position = node.gameObject.transform.position;
+
+			// Update environment extents - minimum and maximum
+			if (position.x < Extents.xMin) Extents.xMin = position.x;
+			if (position.y < Extents.yMin) Extents.yMin = position.y;
+			if (position.x > Extents.xMax) Extents.xMax = position.x;
+			if (position.y > Extents.yMax) Extents.yMax = position.y;
 		};
+
 		//OldNodes.Clear();
 		for(int i = 0; i < Nodes.Count; ++i)
-			OldNodes[i] = Nodes[i];
+			OldNodes[i] = Nodes[i];  // <-- can we move this to the end of the above loop?
 
 //		Edges = Edges ?? new List<Edge>();
 //		for(int i = Edges.Count - 1; i >= 0; --i)
@@ -63,5 +79,7 @@ public class NodeManager : MonoBehaviour {
 //				}
 //			}
 	}
+
+
 }
 
